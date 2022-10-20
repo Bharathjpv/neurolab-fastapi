3b8677d0794f925a130057d648077c09dc1095ff
Bharathjpv <bharathjpv5@gmail.com>
2022-10-20 15:17:34 +0530

    update coments

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
+# Home route
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
+# items route
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
